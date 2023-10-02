6960b44f828df1dd13a6d6fe22bc88a959e63f08
Noah Shoap <nshoap@vols.utk.edu>
2023-10-02 17:04:20 -0400

    basic functionality

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const display = document.querySelector(".text");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const evaluate = document.querySelector("#equal");
+
+let leftNumber = null;
+let rightNumber = null;
+let operator = null;
+
 function add(x, y) {
     return x + y;
 }
@@ -17,17 +26,21 @@ function divide(x, y) {
 function operate(x, y, operator) {
     let result = 0;
 
+    x = Number(x);
+    y = Number(y);
+
+
     switch (operator) {
-        case `+`:
+        case `add`:
             result = add(x, y);
             break;
-        case `-`:
+        case `subtract`:
             result = subtract(x, y);
             break;
-        case '*':
+        case 'multiply':
             result = multiply(x, y);
             break;
-        case `/`:
+        case `divide`:
             result = divide(x,y);
             break;
         default:
@@ -37,3 +50,85 @@ function operate(x, y, operator) {
 
     return result;
 }
+
+function updateDisplay() {
+    // for now, just replace display number with recent number
+    if (operator === null) {
+        display.textContent = `${leftNumber}`;
+    } else {
+        if (rightNumber === null) {
+            display.textContent = `0`;
+        } else {
+            display.textContent = `${rightNumber}`;
+        }
+    }
+}
+
+function numberFromId(id) {
+    switch (id) {
+        case 'nine':
+            return 9;
+        case 'eight':
+            return 8;
+        case 'seven':
+            return 7;
+        case 'six':
+            return 6;
+        case 'five':
+            return 5;
+        case 'four':
+            return 4;
+        case 'three':
+            return 3;
+        case 'two':
+            return 2;
+        case 'one':
+            return 1;
+        case 'zero':
+            return 0;
+        default:
+            alert("bad number");
+    }
+}
+
+numbers.forEach( (numberButton) => {
+    numberButton.addEventListener('click', () => {
+        if (leftNumber === null) {
+            leftNumber = numberFromId(numberButton.id);
+            console.log(typeof(leftNumber));
+        } else if (operator === null) {
+            console.log('number adding to left');
+            leftNumber = `${leftNumber}${numberFromId(numberButton.id)}`;
+        } else {
+            if (rightNumber === null) {
+                rightNumber = numberFromId(numberButton.id);
+            } else {
+                rightNumber = `${rightNumber}${numberFromId(numberButton.id)}`;
+            }
+        }
+
+        updateDisplay();
+    });
+});
+
+operators.forEach( (operatorButton) => {
+    if (operatorButton.id === 'equal') return;
+
+    operatorButton.addEventListener('click', () => {
+        operator = operatorButton.id;
+    });
+});
+
+evaluate.addEventListener('click', () => {
+    console.log(`Left: ${leftNumber}`);
+    console.log(`Operator: ${operator}`);
+    console.log(`Right: ${rightNumber}`);
+
+    leftNumber = operate(leftNumber, rightNumber, operator);
+
+    console.log(`Result: ${leftNumber}`);
+    rightNumber = null;
+    operator = null;
+
+    updateDisplay();
+});
